bd74bc5dbf7980501a928be846203931e4704960
polon59 <przemyslaw.polczak@gmail.com>
2019-02-01 20:29:37 +0100

    Add method to get all users from database

--- JS/dbConn.js
+++ JS/dbConn.js
@@ -10,9 +10,28 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-createUsersTable(connection);
+// createUsersTable(connection);
+// addUserToTable(connection);
+displayUsers(connection);
+
 
 });
+
+function displayUsers(connection) {
+        connection.query("SELECT * FROM users", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
+}
+
+function addUserToTable(connection) {
+    var sql = "INSERT INTO users (login,  password) VALUES ('Rychu', 'rysiek96');";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("user added");
+    });
+}
+
 
 function createUsersTable(connection) {
     var sql = "CREATE TABLE users (login VARCHAR(30) PRIMARY KEY, password VARCHAR(25))";
