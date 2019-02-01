b152ff75fbb40150e6615f87155bae15969c3fac
polon59 <przemyslaw.polczak@gmail.com>
2019-02-01 20:12:15 +0100

    Add functions to create table with users

--- JS/dbConn.js
+++ JS/dbConn.js
@@ -1,12 +1,30 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "mysQLpassword"
+  password: "mysQLpassword",
+  database: "mydb"
 });
 
-con.connect(function(err) {
+connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+createUsersTable(connection);
+
 });
+
+function createUsersTable(connection) {
+    var sql = "CREATE TABLE users (login VARCHAR(30) PRIMARY KEY, password VARCHAR(25))";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+}
+
+function createDatabase(connection) {
+    connection.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+      });
+}
