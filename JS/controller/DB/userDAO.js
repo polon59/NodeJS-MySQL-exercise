c389b6621b3f51e60ef458231c07bbd4664fdf54
polon59 <przemyslaw.polczak@gmail.com>
2019-02-03 11:20:19 +0100

    Change project structure to MVC

--- JS/controller/DB/userDAO.js
+++ JS/controller/DB/userDAO.js
@@ -5,15 +5,18 @@ class UserDAO{
     }
 
     addNewUserToDatabase(login, password){
+        let queryRes = 1;
         let sql = `INSERT INTO users (login,  password) VALUES ('${login}', '${password}');`;
         this.connection.query(sql, function (err, result) {
-          if (err) throw err;
+          if (err) {queryRes = 0;}
+          else {queryRes = 1;}
         });
+        return queryRes;
     }
 
     getAllUsersFromDataBase(){
         this.connection.query("SELECT * FROM users", function (err, result, fields) {
-            if (err) throw err;
+            if (err) throw er;
             console.log(result);
             return result;
           });
