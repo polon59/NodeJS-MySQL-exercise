b9460044e34c4ccae45b9a34b51cf78a1b5f2394
polon59 <przemyslaw.polczak@gmail.com>
2019-02-04 09:32:41 +0100

    Implement empty class 'UserDAO'

--- JS/controller/DB/userDAO.js
+++ JS/controller/DB/userDAO.js
@@ -4,23 +4,7 @@ class UserDAO{
         this.connection = dbConnection;
     }
 
-    addNewUserToDatabase(login, password){
-        let queryRes = 1;
-        let sql = `INSERT INTO users (login,  password) VALUES ('${login}', '${password}');`;
-        this.connection.query(sql, function (err, result) {
-          if (err) {queryRes = 0;}
-          else {queryRes = 1;}
-        });
-        return queryRes;
-    }
-
-    getAllUsersFromDataBase(){
-        this.connection.query("SELECT * FROM users", function (err, result, fields) {
-            if (err) throw er;
-            console.log(result);
-            return result;
-          });
-    }
+    
 
 }
 
